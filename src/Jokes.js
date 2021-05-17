2bac770ba86f047b15fd1fa2f6b5c951a8e55caa
bantale <bantale@kobo360.com>
2021-05-17 16:03:50 +0100

    initial commit

--- src/Jokes.js
+++ src/Jokes.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function Jokes() {
+  const [seconds, setSeconds] = React.useState(20);
   const [joke, setJoke]  = useState({});
   const fetchJokes = async () => {
     try {
@@ -18,6 +19,24 @@ function Jokes() {
       console.log(error);
     }
   };
+  React.useEffect(() => {
+    if (seconds > 0) {
+      setTimeout(() => setSeconds(seconds - 1), 1000);
+    } else {
+      
+    }
+  },[seconds]);
+  // useEffect(() => {
+  //   const timer =
+  //     counter > 0 && setInterval(() => setCounter(counter - 1), 1000);
+  //   return () => clearInterval(timer);
+  // }, [counter]);
+  // if (seconds > 0) {
+  //     setTimeout(() => setSeconds(seconds - 1), 1000);
+  //   } else {
+  //     setSeconds('BOOOOM!');
+  //   }
+
   useEffect(() => {
   const timer = setInterval(() => {
     fetchJokes();
@@ -27,6 +46,7 @@ function Jokes() {
  
   return (
     <div>
+      <div>Seconds: {seconds}</div>
       <p>Joke: {joke? joke.setup : ''}</p>
       <p>punchline: {joke? joke.punchline : ''}</p>
       
