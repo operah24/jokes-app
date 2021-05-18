8732705b28f51a10a52d91176f4b3bb2d9b5ee08
bantale <bantale@kobo360.com>
2021-05-18 10:44:50 +0100

    initial commit

--- src/Jokes.js
+++ src/Jokes.js
@@ -1,55 +1,48 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function Jokes() {
   const [seconds, setSeconds] = React.useState(20);
-  const [joke, setJoke]  = useState({});
+  const [joke, setJoke] = useState({});
   const fetchJokes = async () => {
     try {
-      const url = 'https://official-joke-api.appspot.com/random_joke';
+      const url = "https://official-joke-api.appspot.com/random_joke";
       const config = {
         headers: {
-          Accept: 'application/json',
+          Accept: "application/json",
         },
       };
       const { data } = await axios.get(url, config);
       console.log(data);
       setJoke(data);
+      setSeconds(20);
     } catch (error) {
       console.log(error);
     }
   };
-  React.useEffect(() => {
-    if (seconds > 0) {
-      setTimeout(() => setSeconds(seconds - 1), 1000);
-    } else {
-      
-    }
-  },[seconds]);
-  // useEffect(() => {
-  //   const timer =
-  //     counter > 0 && setInterval(() => setCounter(counter - 1), 1000);
-  //   return () => clearInterval(timer);
-  // }, [counter]);
-  // if (seconds > 0) {
-  //     setTimeout(() => setSeconds(seconds - 1), 1000);
-  //   } else {
-  //     setSeconds('BOOOOM!');
-  //   }
+  useEffect(() => {
+    fetchJokes();
+  }, []);
 
   useEffect(() => {
-  const timer = setInterval(() => {
-    fetchJokes();
-  }, 20000);
-  return () => clearInterval(timer);
-}, []);
- 
+    if (seconds > 0) {
+      setTimeout(() => 
+        setSeconds(seconds - 1), 1000);
+    }
+  }, [seconds]);
+
+  useEffect(() => {
+    if (seconds === 0) {
+      fetchJokes();
+    }
+
+  }, [seconds]);
+
   return (
     <div>
-      <div>Seconds: {seconds}</div>
-      <p>Joke: {joke? joke.setup : ''}</p>
-      <p>punchline: {joke? joke.punchline : ''}</p>
-      
+      <h3>Seconds: {seconds}</h3>
+      <p>Joke: {joke ? joke.setup : ""}</p>
+      <p>punchline: {joke ? joke.punchline : ""}</p>
     </div>
   );
 }
